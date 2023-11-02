577c30f54d0879fbfa6bd6a3b53dd21635ed9244
ABarsen <116378189+ABarsen@users.noreply.github.com>
2023-11-02 19:29:31 +0300

    first commit

---
+++ dom.js
@@ -0,0 +1,8 @@
+//console.log(document.getElementById("main-title"));
+
+/*var headerTitle =document.getElementById('header-title');
+console.log(header-title);*/
+//headerTItle.innerHTML= "<h1>hello</h1>";
+var items=document.getElementsByClassName ('list-groups');
+console.log(items);
+console.log(items[0]);
