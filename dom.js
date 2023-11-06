cd4951438ef3c3f89e7c43a7b0895dedd54dcbe9
ABarsen <116378189+ABarsen@users.noreply.github.com>
2023-11-06 07:35:25 +0300

    first changes

--- dom.js
+++ dom.js
@@ -1,8 +1,12 @@
-//console.log(document.getElementById("main-title"));
+var  NewDiv = document.createElement('div');
+//add class
+NewDiv.className= 'hello';
+//add id
+ 
+NewDiv.id ='hello1';
 
-/*var headerTitle =document.getElementById('header-title');
-console.log(header-title);*/
-//headerTItle.innerHTML= "<h1>hello</h1>";
-var items=document.getElementsByClassName ('list-groups');
-console.log(items);
-console.log(items[0]);
+//add atribute 
+NewDiv.setAttribute('title', 'hello div');
+ console.log (NewDiv);
+ NewDiv.style.dontsize ='30px'
+ 
